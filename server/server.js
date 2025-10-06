b9a7373befa98ef781b456854bd6150e19fac35d
khaleddaidar012 <khaleddaidar2@gmail.com>
2025-10-06 07:32:04 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -108,7 +108,13 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+/*const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+});*/
+
+
+
+// ✅ التصدير لفِرسال
+export default app;
+

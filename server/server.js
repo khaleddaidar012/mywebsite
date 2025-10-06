152dbb0d77e1406dfac5bed53a43dc79e15c017c
khaleddaidar012 <khaleddaidar2@gmail.com>
2025-10-06 07:22:06 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -97,6 +97,16 @@ app.delete("/api/projects/:id", async (req, res) => {
     res.status(500).json({ message: "Could not delete project" });
   }
 });
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

63d901f5b7c67f28aaf6010c0d999444c989c5b5
khaleddaidar <khaleddaidar@gmail.com>
2026-03-28 19:16:25 +0200

    some edits commit

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -113,7 +113,7 @@ export const ContactSection = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4">Contact With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
+                <a href="https://www.linkedin.com/in/khaled-daidar-909316218/" target="_blank">
                   <Linkedin />
                 </a>
                 

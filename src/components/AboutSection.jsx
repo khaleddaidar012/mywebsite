dd37c6fc79eeb9e7f70bb51a3e314bb31fadf36e
khaleddaidar <khaleddaidar@gmail.com>
2026-05-01 09:12:50 +0300

    edits

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,7 +1,20 @@
 import { Briefcase, Code, User } from "lucide-react";
 
 export const AboutSection = () =>{
+<div className="flex flex-col sm:flex-row gap-4 pt-4">
+  
+  <a href="#contact" className="cosmic-button">
+    Get In Touch
+  </a>
 
+  <button
+    onClick={handleDownload}
+    className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+  >
+    Download CV
+  </button>
+
+</div>
     return(
         <section id="about" className="py-24 px-4 relative">
             {" "}
@@ -22,13 +35,20 @@ Passionate about developing innovative and simple solutions to complex problems,
 
                         </p>
                         <div className="flex flex-col sm:flex-row gap-4 pt-4 .justify-center">
-                            <a href="#contact" className="cosmic-button">
-                                {" "}
-                                Get In Touch
-                            </a>
-                            <a href="https://drive.google.com/file/d/1g5DkKFnxYzJWgaRfWn2HE2LyMizUeWcD/view?usp=sharing" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
-                            Download CV
-                            </a>
+                          <div className="flex flex-col sm:flex-row gap-4 pt-4">
+  
+  <a href="#contact" className="cosmic-button">
+    Get In Touch
+  </a>
+
+  <button
+    onClick={handleDownload}
+    className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+  >
+    Download CV
+  </button>
+
+</div>
                         </div>
                     </div>
 
